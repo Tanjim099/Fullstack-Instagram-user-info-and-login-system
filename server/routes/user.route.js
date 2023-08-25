6c14a93b5df23f7b0f6222dae16c4347ed7efccd
Tanjim <tanjimseo@gmail.com>
2023-08-25 11:43:34 +0530

    Backend and Frontend Register page done

---
+++ server/routes/user.route.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { signUpValidator } = require("../middleware/signup.validator");
+const { loginValidator } = require("../middleware/login.validator");
+const { authenticateUser } = require("../middleware/authenticateUser");
+const { userSignUp, userLogin, getUserDetails } = require("../controller/user.controller");
+
+const userRoute = express.Router();
+
+userRoute.post("/signup", signUpValidator, userSignUp);
+
+userRoute.post("/login", loginValidator, userLogin);
+
+userRoute.get("/username", authenticateUser, getUserDetails);
+
+module.exports = {
+    userRoute
+}
---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const connectDatabase = require("./config/db");
+const { userRoute } = require("./routes/user.route");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use(cookieParser())
+app.use("/", userRoute)
+
+app.listen(process.env.PORT, async () => {
+    // connectDatabase()
+    connectDatabase()
+    console.log("Server Starting on PORT :", process.env.PORT)
+})
